f632eb29a8240bfc2f602359ecae493133c8eaab
Rohnin_B <90275692+Rohnin-Barrette@users.noreply.github.com>
2021-10-06 12:11:47 -0400

    Update code.py

--- code.py
+++ code.py
@@ -1,1 +1,22 @@
-print("Hello, World!")
+#!/usr/bin/env python3
+
+# Created by:  Rohin Barrette
+# Created on: october 2021
+# This program is the "space aliens" program on the pybadge
+
+import ugame
+import stage
+
+
+def game_scene():
+    # this fuction is the main game_scene
+    
+    print("\n\n\n") # 3 blank lines
+    print("Hello, World!")
+    
+    # repeat forever in game loop
+    while True:
+        pass # just a placeholder for now
+    
+if __name__ == "__main__":
+    game_scene()
