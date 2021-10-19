5c7e0c9e455b43d5a7acc0cca38c4a6e90a5c40c
Rohnin_B <90275692+Rohnin-Barrette@users.noreply.github.com>
2021-10-18 21:43:06 -0400

    Update code.py

--- code.py
+++ code.py
@@ -10,6 +10,64 @@ import stage
 import constants
 
 
+def menu_scene():
+    # function is the main game game_scene
+    
+    #image banks for CircutPython
+    image_bank_mt_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
+    
+    text = []
+    text1 = stage.Text(width=29, height=12, font=None, palette=constants.RED_PALETTE, buffer=None)
+    text1.move(20, 10)
+    text1.text("MT Game Studios")
+    text.append(text1)
+    
+    text2 = stage.Text(width=29, height=12, font=None, palette=constants.RED_PALETTE, buffer=None)
+    text2.move(40, 110)
+    text2.text("PRESS START")
+    text.append(text2)
+    
+    
+    
+    # sets the background to image 0 in the image Bank
+    #   and the size (10x8 tiles of the size 16x16)
+    background = stage.Grid(image_bank_mt_background, constants.SCREEN_GRID_X,
+                            constants.SCREEN_GRID_Y)
+    
+    #create a stage for the background to show up on
+    #   and set the frame rate to 60fps
+    game = stage.Stage(ugame.display, constants.FPS)
+    # set the layers of all sprites, items show up in order
+    game.layers = text + [background]
+    # render all sprites
+    #   most likely you will only render the background once per game scene
+    game.render_block()
+    
+    # sets the background to image 0 in the image bank
+    background = stage.Grid(image_bank_mt_background, constants.SCREEN_X,
+                            constants.SCREEN_Y)
+                            
+    # create a stage for the background to show up once
+    #    and set the frame rate to 60fps
+    game = stage.Stage(ugame.display, constants.FPS)
+    # set the layers, items show up in order
+    game.layers = text + [background]
+    # render the background and initial location of sprite list
+    # most likely only render background once per scene
+    game.render_block()
+    
+    # repeat forever, game loop
+    while True:
+        # get user input
+        keys = ugame.buttons.get_pressed()
+        
+        
+        if keys & ugame.K_START != 0:
+            game_scene()
+
+        # redraw Sprite 
+        game.tick() # wait until refresh rate finishes
+        
 def game_scene():
     # function is the main game game_scene
     
@@ -98,7 +156,8 @@ def game_scene():
         # redraw Sprite 
         game.render_sprites([ship] + [alien])
         game.tick() # wait until refresh rate finishes
+  
         
 if __name__ == "__main__":
-    game_scene()
+    menu_scene()
     
