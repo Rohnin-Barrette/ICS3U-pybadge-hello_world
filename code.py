0f5d6d13c6dfc84f55089c47a0797559e0b5990f
Rohnin_B <90275692+Rohnin-Barrette@users.noreply.github.com>
2021-10-19 21:50:45 -0400

    Update code.py

--- code.py
+++ code.py
@@ -6,10 +6,77 @@
 
 import ugame
 import stage
+import time 
+import random
 
 import constants
 
 
+def splash_scene():
+    # function is the splash scene
+    
+    # get sound ready
+    coin_sound = open("coin.wav", 'rb')
+    sound = ugame.audio
+    sound.stop()
+    sound.mute(False)
+    sound.play(coin_sound)
+    
+    #image banks for CircutPython
+    image_bank_mt_background = stage.Bank.from_bmp16("mt_game_studio.bmp")
+    
+
+    # create a stage for background to show up on
+    #   and set frame rate to 60fps
+    # sets the background to image 0 in the image Bank
+    background = stage.Grid(image_bank_mt_background, constants.SCREEN_GRID_X,
+                            constants.SCREEN_GRID_Y)
+                            
+       # used this program to split the image into tile: 
+    #   https://ezgif.com/sprite-cutter/ezgif-5-818cdbcc3f66.png
+    background.tile(2, 2, 0)  # blank white
+    background.tile(3, 2, 1)
+    background.tile(4, 2, 2)
+    background.tile(5, 2, 3)
+    background.tile(6, 2, 4)
+    background.tile(7, 2, 0)  # blank white
+
+    background.tile(2, 3, 0)  # blank white
+    background.tile(3, 3, 5)
+    background.tile(4, 3, 6)
+    background.tile(5, 3, 7)
+    background.tile(6, 3, 8)
+    background.tile(7, 3, 0)  # blank white
+
+    background.tile(2, 4, 0)  # blank white
+    background.tile(3, 4, 9)
+    background.tile(4, 4, 10)
+    background.tile(5, 4, 11)
+    background.tile(6, 4, 12)
+    background.tile(7, 4, 0)  # blank white
+
+    background.tile(2, 5, 0)  # blank white
+    background.tile(3, 5, 0)
+    background.tile(4, 5, 13)
+    background.tile(5, 5, 14)
+    background.tile(6, 5, 0)
+    background.tile(7, 5, 0)  # blank white
+    
+    #create a stage for the background to show up on
+    #   and set the frame rate to 60fps
+    game = stage.Stage(ugame.display, constants.FPS)
+    # set the layers of all sprites, items show up in order
+    game.layers = [background]
+    # render all sprites
+    #   most likely you will only render the background once per game scene
+    game.render_block()
+    
+    # repeat forever, game loop
+    while True:
+        # Wait 2 seconds
+        time.sleep(2.0)
+        menu_scene()
+    
 def menu_scene():
     # function is the main game game_scene
     
@@ -66,8 +133,8 @@ def menu_scene():
             game_scene()
 
         # redraw Sprite 
-        game.tick() # wait until refresh rate finishes
-        
+        game.tick() # wait until refresh rate finishes     
+   
 def game_scene():
     # function is the main game game_scene
     
@@ -159,5 +226,5 @@ def game_scene():
   
         
 if __name__ == "__main__":
-    menu_scene()
+    splash_scene()
     
